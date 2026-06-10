8afbaa35d40c6ed536a7b1ed2185253aee9387da
JanhaviSingh2004 <janhavisinghchurk@gmail.com>
2026-06-11 01:15:34 +0530

    render deploy version

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import docker
 
 from backend.carbon_api import get_carbon_intensity
 
@@ -9,31 +8,18 @@ API_KEY = "EwDxzHQuhmGJYDz46Xys"
 app = Flask(__name__)
 CORS(app)
 
-# Docker client
-client = docker.from_env()
-
-# ✅ ADD THIS FUNCTION
-def get_container_status():
-    containers = client.containers.list(all=True)
-
-    running = []
-    stopped = []
-
-    for c in containers:
-        if c.status == "running":
-            running.append(c.name)
-        else:
-            stopped.append(c.name)
-
-    return running, stopped
-
 
 @app.route("/status")
 def status():
 
     carbon = get_carbon_intensity(API_KEY)
 
-    running, stopped = get_container_status()
+    if carbon and carbon > 600:
+        running = ["analytics-job"]
+        stopped = ["ml-training", "backup-job"]
+    else:
+        running = ["analytics-job", "ml-training", "backup-job"]
+        stopped = []
 
     return jsonify({
         "carbon_intensity": carbon,
